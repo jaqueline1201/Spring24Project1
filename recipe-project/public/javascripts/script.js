aa5e76ed06d32351cea6b3f570c2ff1a6f63f1d4
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-05-20 15:57:00 -0700

    some changes in the javascript page2

--- recipe-project/public/javascripts/script.js
+++ recipe-project/public/javascripts/script.js
@@ -1,11 +1,78 @@
-// const recipesInformation = [];
+const recipesInformation = [];
 
-// const recipe = function (rTitle,rType,rSource,rIngredients,rInstructions,rLink,rNotes) {
-//     this.title = rTitle;
-//     this.type = rType;
-//     this.source = rSource;
-//     this.ingredientes = rIngredients;
-//     this.instructions = rInstructions;
-//     this.link = rLink;
-//     this.notes = rNotes;
-// }
+const recipe = function (rTitle,rType,rSource,rIngredients,rInstructions,rLink,rNotes) {
+    this.title = rTitle;
+    this.type = rType;
+    this.source = rSource;
+    this.ingredientes = rIngredients;
+    this.instructions = rInstructions;
+    this.link = rLink;
+    this.notes = rNotes;
+}
+
+function addRecipe () {
+    let isValid = true;
+
+    if (!isTitleValid()) {
+        isValid = false;
+    }
+    if (!isTypeOfFoodValid ()) {
+        isValid = false;
+    }
+    if (!isIngredientsValid()) {
+        isValid = false;
+    }
+    if (!isInstructionsValid()) {
+        isValid = false;
+    }
+
+    return false;
+    
+}
+
+function isTitleValid () {
+    let recipeTitle = document.getElementById("recipeTitle").value;
+    if (recipeTitle == "") {
+        document.getElementById("recipeTitleError").innerHTML = "Title of the recipe is requiered!"
+    } else {
+        document.getElementById("recipeTitleError").innerHTML = ""
+        return true;
+    }
+}
+
+function isTypeOfFoodValid () {
+    let typeOfFood = document.getElementById("typeOfFood").value;
+    if (typeOfFood == "none") {
+        document.getElementById("typeOfFoodError").innerHTML = "Please choose a type of Food!"
+    } else {
+        document.getElementById("typeOfFoodError").innerHTML = ""
+        return true;
+    }
+}
+
+//function of the fieldset goes here//
+
+function isIngredientsValid () {
+    let ingredients = document.getElementById("ingredients").value;
+    if (ingredients == "") {
+        document.getElementById("ingredientsError").innerHTML = "Don't forget to list your ingredients!"
+    } else {
+        document.getElementById("ingredientsError").innerHTML = ""
+        return true;
+    }
+}
+
+function isInstructionsValid () {
+    let instructions = document.getElementById("instructions").value;
+    if (instructions == "") {
+        document.getElementById("instructionsError").innerHTML = ""
+        return true;
+    }
+}
+
+function save ()
+
+function clearList () {
+    let recipeForm = document.getElementById("recipeForm");
+    movieList.innerHTML = "";
+}
