66e59135cceb0f7aa09f67eaaaf55335d4c9867b
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-06-09 01:06:14 -0700

    list of recipies working fine

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -3,9 +3,15 @@ $(document).ready(function(){
         type: "GET",
         url:"/recipes",
         dataType:"json",
-        success:function(data){
-            console.log(data);
+        success: function(data) {
+            const recipes = data.recipeList;
+            recipes.forEach(function(value,index) {
+                console.log(value);
+                $("#recipeContainer").append(`
+                <li><a a href="#page4">${value.title}</a></li> 
+                `);
+            });
+            
         }
-
     })
 })
